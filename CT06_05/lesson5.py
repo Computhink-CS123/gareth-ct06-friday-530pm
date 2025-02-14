c72bb796bb9b9300d2628e6584090026e6013ca5
Computhink-CS123 <cs123@computhink.com.sg>
2025-02-14 10:11:34 +0000

    Feb 14, 2025, 10:11 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -2,7 +2,7 @@
 #bdayage = input("what is the age of the birthday person?")
 #bdaymes = input("what is the pesonalised message the birthday person wants to say?")
 #print (str(bdayname) + " is now " + str(bdayage) + " and invites you to their birthday party!The more the merrier." + str(bdaymes))
-print ("before the loop")
-for counter in range(5):
+#print ("before the loop")
+#for counter in range(5):
     print ("inside the loop")
 print ("after the loop")
