275e5e54f911f7e13135f1ac804e681c1fed1096
Computhink-CS123 <cs123@computhink.com.sg>
2025-02-14 10:43:48 +0000

    Feb 14, 2025, 10:43 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,4 +1,4 @@
-#bdayname = input("what is the name of the birthday person?")
+#bdayname = input("whfor mun in range(51,100):at is the name of the birthday person?")
 #bdayage = input("what is the age of the birthday person?")
 #bdaymes = input("what is the pesonalised message the birthday person wants to say?")
 #print (str(bdayname) + " is now " + str(bdayage) + " and invites you to their birthday party!The more the merrier." + str(bdaymes))
@@ -23,7 +23,7 @@
 #    print(num)
 #for num in range(1,5):
 #    print(num)
-#for mun in range(51,100):
+#
 #    print(mun)
 #for nmu in range(18,29):
 #    print(nmu)
