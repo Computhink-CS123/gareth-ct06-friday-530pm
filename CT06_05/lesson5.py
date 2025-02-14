983a9e3696eb9275562d58be1aee4778ebe7a2ff
Computhink-CS123 <cs123@computhink.com.sg>
2025-02-14 09:55:22 +0000

    Feb 14, 2025, 9:55 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,4 +1,4 @@
 bdayname = input("what is the name of the birthday person?")
 bdayage = input("what is the age of the birthday person?")
 bdaymes = input("what is the pesonalised message the birthday person wants to say?")
-print (str(bdayname) + " is now " " invites you to their birthday party!The more the merrier." + str(bdaymes))
+print (str(bdayname) + " is now " +  " invites you to their birthday party!The more the merrier." + str(bdaymes))
