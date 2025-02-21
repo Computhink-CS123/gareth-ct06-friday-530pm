811aa10e1fa3e118834369e5059058e96a1cc135
Computhink-CS123 <cs123@computhink.com.sg>
2025-02-21 10:52:02 +0000

    Feb 21, 2025, 10:52 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -65,4 +65,4 @@ for count in range(tries):
     if reply < hidden:
         print("guess a bigger number")
     elif reply > hidden:
-        print ("guess a smaal")
+        print ("guess a smaller number")
