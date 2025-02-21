927b5a15bb866feb76ca99b56a5226526d45331b
Computhink-CS123 <cs123@computhink.com.sg>
2025-02-21 09:51:17 +0000

    Feb 21, 2025, 9:51 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,11 +1,11 @@
-num1 = input("give me a number")
-num2 = input("give me a number")
-num1 = int(num1)
-num2 = int(num2)
-if num1 > num2:
-    temp = num1
-    num1 = num2
-    num2 = temp
+#num1 = input("give me a number")
+#num2 = input("give me a number")
+#num1 = int(num1)
+#num2 = int(num2)
+#if num1 > num2:
+#    temp = num1
+#    num1 = num2
+#    num2 = temp
 
 for count in range(num1,num2):
     print(count)
