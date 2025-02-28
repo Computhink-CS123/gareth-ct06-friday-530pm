a953f469189305792aa7efcea6d45268e7ab1577
Computhink-CS123 <cs123@computhink.com.sg>
2025-02-28 09:34:10 +0000

    Feb 28, 2025, 9:34 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,2 +1,12 @@
+score_one = 80
+score_two = 90
+score_three = 75
 
+total = score_one + score_two + score_three
+
+average_score = totl / 3
+
+student_name = "Alex"
+
+print("Average score for " + student_name + " is: " + average_score)
 
