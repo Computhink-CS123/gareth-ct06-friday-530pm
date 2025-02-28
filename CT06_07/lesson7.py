51ed132c8a38b7baf3a9fee32cacc6860506bf30
Computhink-CS123 <cs123@computhink.com.sg>
2025-02-28 10:21:16 +0000

    Feb 28, 2025, 10:21 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -34,7 +34,7 @@ num2 = 0
 for char in range(1,num1):
     num = input("What is number #" + str(char) + "?")
     num = int(num)
-    num3 = (num2 + num)
+    num3 = num2 + num
     num2 = num3
 print = (num2)
 
