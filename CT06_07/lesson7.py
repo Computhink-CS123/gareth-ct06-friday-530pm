b33080a928c16033fd6a4789887977612ec3a582
Computhink-CS123 <cs123@computhink.com.sg>
2025-02-28 10:34:10 +0000

    Feb 28, 2025, 10:34 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -32,7 +32,7 @@
 num1 = 6
 num2 = 0
 for char in range(1,num1):
-    num = input("What is number #" , char , "")
+    num = input("What is number #" , char , "? ")
     num = int(num)
     num2 = num + num2
 print = ("the total number is" , num2)
