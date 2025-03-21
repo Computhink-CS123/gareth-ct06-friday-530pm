893afa691ca7296633ed4723d03c476a3ce12a94
Computhink-CS123 <cs123@computhink.com.sg>
2025-03-21 10:01:56 +0000

    Mar 21, 2025, 10:01 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -51,5 +51,6 @@ import random
 
 num1 = random.randint(1,50)
 print ("random number is " + str(num1))
-start = int(input("start number?))
+start = int(input("start number?"))
+
 print( start <= num1 <= end )
