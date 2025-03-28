7ede904a5c37838d57fa06570ef35558ed8f550d
Computhink-CS123 <cs123@computhink.com.sg>
2025-03-28 09:55:31 +0000

    Mar 28, 2025, 9:55 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,9 +1,9 @@
-numapple = int(input("How many apples did you buy? "))
-numorange = int(input("How many oranges did you buy? "))
-if numapple > 5:
-    appleprice = numapple * 0.6 * 0.9
-else:
-    appleprice = numapple * 0.6
+#numapple = int(input("How many apples did you buy? "))
+#numorange = int(input("How many oranges did you buy? "))
+#if numapple > 5:
+#    appleprice = numapple * 0.6 * 0.9
+#else:
+#    appleprice = numapple * 0.6
 
 if numorange > 5:
     orangeprice = numorange * 0.9 * 0.9
