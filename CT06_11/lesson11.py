5f8c6ba01e3e55c7c5871c3b1829c166bb2facf7
Computhink-CS123 <cs123@computhink.com.sg>
2025-04-04 10:12:14 +0000

    Apr 4, 2025, 10:12 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -3,4 +3,4 @@ if px <= 5:
     print("Sounds good!")
 elif  px <= 50: 
     print("Are you sure you need this?")
-elif px <= 500: "Where are you getting this money from?!"
+elif px <= 500: ("Where are you getting this money from?!")
