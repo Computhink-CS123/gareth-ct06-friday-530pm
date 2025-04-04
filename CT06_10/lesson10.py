3dcdb8d9448b6ee16f557aaa4db433e3b7da53bd
Computhink-CS123 <cs123@computhink.com.sg>
2025-04-04 09:59:42 +0000

    Apr 4, 2025, 9:59 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -6,14 +6,14 @@
 #else:
 #    print("adult")
 
-score = int(input("what is your score?"))
-if 89 < score < 101 :
-    print("Grade A")
-elif 79 < score < 90:
-    print("Grade B")
-elif 69 < score < 80:
-    print("Grade C")
-elif 59 < score < 70:
-    print("Grade D")
-else:
+#score = int(input("what is your score?"))
+#if 89 < score < 101 :
+#    print("Grade A")
+#elif 79 < score < 90:
+#    print("Grade B")
+#elif 69 < score < 80:
+#    print("Grade C")
+#elif 59 < score < 70:
+#    print("Grade D")
+#else:
     print("Grade F")
