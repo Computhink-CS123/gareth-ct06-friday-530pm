5db720c04169c00bed4e78a64c573429cc1ac65a
Computhink-CS123 <cs123@computhink.com.sg>
2025-04-11 10:02:03 +0000

    Apr 11, 2025, 10:02 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -33,4 +33,22 @@
 #gender = input("to enter this SKIBIDI TOILET, Please enter your GEN_deR. enter M/Male if male gender")
 #if gender == m or gender == male:
 
-    
+#colour = input("guess a colour")
+#if not (colour == "green") :
+#  print("try again")
+#else:
+#  print("correct")
+
+#day = input("what day is it?")
+#if not (day == "saturday"or"sunday"):
+#  print ("it is the week end.yaaaaaay! ")
+#else:
+#  print("it is not the weekend end. booooo")
+
+burger = input("do you want a burger?")
+fries = input("do you want a fries?")
+drink = input("do you want a drink?")
+if burger == "yes" and fries == "yes" and not drink == "yes":
+  print ("won't u get thirsty?")
+else:
+  print("order submited.  -__-")
