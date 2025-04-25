544000b462499b4e2e64f510cf47a91ba35f0261
Computhink-CS123 <cs123@computhink.com.sg>
2025-04-25 10:08:17 +0000

    Apr 25, 2025, 10:08 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -39,7 +39,10 @@ for char in range(10):
         ans = input("what is " + str(num1) + " - " + str(num2))
         ans = int(ans)
         truans = num1 - num2
-    
+    if raneq == 1:
+        ans = input("what is " + str(num1) + " x " + str(num2))
+        ans = int(ans)
+        truans = num1 + num2
     if not ans == truans:
         print ("wrong")
     else :
