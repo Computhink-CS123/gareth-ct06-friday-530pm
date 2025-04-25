e0c86febdd95bfded50051c79023c4907fcb798d
Computhink-CS123 <cs123@computhink.com.sg>
2025-04-25 10:09:28 +0000

    Apr 25, 2025, 10:09 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -32,15 +32,15 @@ for char in range(10):
     num2 = random.randint(1,10)
     raneq = random.randint(1,3)
     if raneq == 1:
-        ans = input("what is " + str(num1) + " + " + str(num2 ))
+        ans = input("what is " + str(num1) + " + " + str(num2))
         ans = int(ans)
         truans = num1 + num2
     if raneq == 2:
-        ans = input("what is " + str(num1) + " - " + str(num2 ))
+        ans = input("what is " + str(num1) + " - " + str(num2))
         ans = int(ans)
         truans = num1 - num2
     if raneq == 1:
-        ans = input("what is " + str(num1) + " x " + str(num2 ))
+        ans = input("what is " + str(num1) + " x " + str(num2))
         ans = int(ans)
         truans = num1 * num2
     if not ans == truans:
