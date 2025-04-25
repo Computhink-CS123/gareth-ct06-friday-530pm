d24b283851f56efde41188d21400306ca091ff3b
Computhink-CS123 <cs123@computhink.com.sg>
2025-04-25 10:35:25 +0000

    Apr 25, 2025, 10:35 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,3 +1,3 @@
 cash = 1000
 while True:
-    ask = input("for (withdraw, press 1), (deposit, press 2), (check balance)")
+    ask = input("for (withdraw, press 1), (deposit, press 2), (check balance, press3 )")
