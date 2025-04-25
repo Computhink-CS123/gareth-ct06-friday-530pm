c44f0827b7b843aa92aa57f9119160a30e19d42f
Computhink-CS123 <cs123@computhink.com.sg>
2025-04-25 09:13:32 +0000

    Apr 25, 2025, 9:13 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -3,11 +3,24 @@
 #    visitors += 1
 #    print(visitors)
 #
-visitor1 = 18
-while visitor1 <30:
-    visitor1 += 1
-    print(visitor1)
-visitor2 = 4
-while visitor2 <25:
-     visitor2 += 1
-     print(visitor2)
+#visitor1 = 18
+#while visitor1 <30:
+#    visitor1 += 1
+#    print(visitor1)
+#visitor2 = 4
+#while visitor2 <25:
+#     visitor2 += 1
+#     print(visitor2)
+
+#visitors = 0
+#while visitors < 50:
+#    print(visitors)
+#    if visitors == 30:
+#        break
+#    visitors += 1
+
+order = ("")
+while True:
+    ans = input("what is your order?")
+    if ans == "end":
+        break
