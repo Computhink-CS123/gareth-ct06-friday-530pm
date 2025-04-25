bf5b4c849db3f94597c8a418e558f80f6ab3c51d
Computhink-CS123 <cs123@computhink.com.sg>
2025-04-25 10:39:49 +0000

    Apr 25, 2025, 10:39 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,9 +1,8 @@
 cash = 1000
 while True:
     ask = input("for (withdraw, press 1), (deposit, press 2), (check balance, press 3), (exit, press 4)")
-    ask = int
-    if ask == 3:
+    if ask == "3":
         print ("your balance is " , cash)
-    elif ask == 4:
+    elif ask == 4":
         print ("thank you!")
         break
