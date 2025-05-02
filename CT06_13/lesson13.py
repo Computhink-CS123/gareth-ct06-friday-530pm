944aa085f527aa2839637f8cf8b80ba2d17ece6d
Computhink-CS123 <cs123@computhink.com.sg>
2025-05-02 09:34:18 +0000

    May 2, 2025, 9:34 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -14,4 +14,4 @@ while True:
         else:
             print("Err0r!Insufision balance!")
     elif ask == "2":
-        depo = int(input("how much money would you like to deposite"))
+        depo = int(input("how much money would you like to deposit?"))
