67e57affa7699da2a0467409239d3b4c8cf771e8
Computhink-CS123 <cs123@computhink.com.sg>
2025-05-09 09:46:29 +0000

    May 9, 2025, 9:46 AM

--- test.py
+++ test.py
@@ -50,7 +50,7 @@ planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
 
 # 1. Write code below to print the 3rd item 
 #    in this list using index e.g. earth
-print(planets)
+print(planets[3])
 
 # 2. Write code to append neptune to this list.
 
