4e10a693009fc2ec93248d87bc7cebb5d45db489
Computhink-CS123 <cs123@computhink.com.sg>
2025-07-07 04:03:34 +0000

    2025-07-07

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,21 +1,21 @@
-#cash = 1000
-#while True:
-#    ask = input("for (withdraw, press 1), (deposit, press 2), (check balance, press 3), (exit, press 4) ")
-#    if ask == "3":
-##        print ("your balance is $" , cash)
-#    elif ask == "4":
-#        print ("thank you!")
-#        break
-#    elif ask == "1":
-#        draw = int(input("how much money would you like to withdraw? "))
-#        if draw <= cash:
-#            cash = cash - draw
-#            print("here is your money")
-#        else:
-#            print("Err0r!Insufision balance!")
-#    elif ask == "2":
-#        depo = int(input("how much money would you like to deposit?"))
-#        cash = cash + depo
+cash = 1000
+while True:
+    ask = input("for (withdraw, press 1), (deposit, press 2), (check balance, press 3), (exit, press 4) ")
+    if ask == "3":
+        print ("your balance is $" , cash)
+    elif ask == "4":
+        print ("thank you!")
+        break
+    elif ask == "1":
+        draw = int(input("how much money would you like to withdraw? "))
+        if draw <= cash:
+            cash = cash - draw
+            print("here is your money")
+        else:
+            print("Err0r!Insufision balance!")
+    elif ask == "2":
+        depo = int(input("how much money would you like to deposit?"))
+        cash = cash + depo
 
 
 #groceries = ["Apples", "Bread","Carrots","Dates","Eggs","Flour","Grapes","Honey"]
